6f5075020ec872d5b835ef25ee3c1fd3ac403b88
manuelgeek <emashmagak@appslab.co.ke>
2020-03-01 21:07:33 +0300

    updates mix.exs

--- mix.exs
+++ mix.exs
@@ -12,8 +12,8 @@ defmodule Mpesa.MixProject do
       deps: deps(),
       # Docs
       name: "Mpesa STK",
-      source_url: "https://github.com/USER/PROJECT",
-      homepage_url: "http://YOUR_PROJECT_HOMEPAGE",
+      source_url: "https://github.com/manuelgeek/mpesa_elixir.git",
+      homepage_url: "https://github.com/manuelgeek/mpesa_elixir.git",
       docs: [
         # The main page in the docs
         main: "Mpesa STK",
@@ -28,7 +28,7 @@ defmodule Mpesa.MixProject do
     [
       maintainers: [" ManuEl Magak "],
       licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/blackode/printex"}
+      links: %{"GitHub" => "https://github.com/manuelgeek/mpesa_elixir.git"}
     ]
   end
 
@@ -44,7 +44,8 @@ defmodule Mpesa.MixProject do
     [
       {:poison, "~> 3.1"},
       {:httpoison, "~> 1.6"},
-      {:timex, "~> 3.5"}
+      {:timex, "~> 3.5"},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
